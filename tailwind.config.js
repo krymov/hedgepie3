9bb349bdc31d691a8f1c2aa5605dfb768a7617de
Mark Krymov <krymov@gmail.com>
2022-02-09 20:58:59 +0100

    the first commit

---
+++ tailwind.config.js
@@ -0,0 +1,78 @@
+const path = require("path");
+const colors = require("tailwindcss/colors");
+const typography = require("@tailwindcss/typography");
+
+const purgecss = require("@fullhuman/postcss-purgecss")({
+  content: ["./hugo_stats.json"],
+  defaultExtractor: (content) => {
+    let els = JSON.parse(content).htmlElements;
+    return els.tags.concat(els.classes, els.ids);
+  },
+});
+
+module.exports = {
+  purge: {
+    enabled: process.env.HUGO_ENVIRONMENT === "production",
+    content: [
+      // path.resolve(__dirname) + "/layouts/**/*.html",
+      "./hugo_stats.json",
+      "./themes/launchist-theme/layouts/**/*.html",
+      // "./content/**/*.md",
+      // "./content/**/*.html",
+    ],
+    extractors: [
+      {
+        extractor: (content) => {
+          let els = JSON.parse(content).htmlElements;
+          return els.tags.concat(els.classes, els.ids);
+        },
+        extensions: ["json"],
+      },
+    ],
+    mode: "all",
+    options: {
+      keyframes: true,
+    },
+  },
+  darkMode: "class", // or 'media' or 'class'
+  theme: {
+    letterSpacing: {
+      widest: '.25em',
+    },
+    fontFamily: {
+      sans: ['"Noto Sans"', 'sans-serif'],
+      display: ['"Poppins"', 'sans-serif']
+    },
+    extend: {
+      colors: {
+        primary: colors.blue,
+        secondary: colors.orange,
+        neutral: colors.gray,
+        cardGrey: '#F6FAFD',
+        curiousBlue: "#1799DE",
+        bleuDeFrance: "#1f7fde",
+        darkishPink: "#de4686",
+        salomie: "#EFA906",
+        haiti: "#160E3A",
+        darkElegant: "$16103A"
+
+      },
+      backgroundImage: {
+        'hero-pattern': "url('https://res.cloudinary.com/dalyoko/f_auto/hedgepie/Background.jpg')",
+        'coin-pattern': "url('https://res.cloudinary.com/dalyoko/f_auto/hedgepie/Frame_1000002141.png')",
+        'background-gradient': "url('https://res.cloudinary.com/dalyoko/f_auto/hedgepie/Background-gradient.png')"
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/typography"),
+    require("tailwindcss"),
+    require("autoprefixer"),
+    ...(process.env.HUGO_ENVIRONMENT === "production" ? [purgecss] : []),
+  ],
+};
